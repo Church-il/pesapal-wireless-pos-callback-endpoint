c269eb3db32580916492be95666e952fa8a6c91f
MichaelAkoko <michael.akoko@pesapal.com>
2025-08-11 12:38:48 +0300

    insert changes

--- callback_listener/app.py
+++ callback_listener/app.py
@@ -13,7 +13,7 @@ DB_TYPE = os.getenv("DB_TYPE")
 if not DB_TYPE:
     DB_TYPE = os.environ.get("DB_TYPE")  # fallback to env var set by Render or Docker
 
-print("DB_TYPE:", DB_TYPE)
+#print("DB_TYPE:", DB_TYPE)
 
 EAT = timezone(timedelta(hours=3))
 
